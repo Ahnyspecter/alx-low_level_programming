fa8fd8f7d149f66ba74d56b847c98c952fd8bd89
ahnspecter <mnak@gmail.com>
2023-02-21 21:35:17 -0800

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the ebem-valued tearms
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	usigned long int j, k,  next, sum;
+
+	
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	
+	
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j = k;
+		j = k;
+		k = next;
+	}
+	
+	
+	print("%lu\n", sum);
+	
+	
+	return (0)
+}
