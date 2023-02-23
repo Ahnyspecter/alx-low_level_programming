cd487c7dd0d2d252384c08096c655696d725a248
ahnspecter <mnak@gmail.com>
2023-02-22 18:03:33 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,10 @@
 /**
  * _isalpha - checks for alphabetic character.
  * @c: the character to be checked.
+ *
  * Return: 1 if c is a letter, 0 otherwise.
  */
-int_isalph(int c)
+int _isalph(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= Z'));
 }
