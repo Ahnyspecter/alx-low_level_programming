94232e8808a063a51a6acef815f0558698cd7de8
ahnspecter <mnak@gmail.com>
2023-02-22 18:57:36 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,11 +5,12 @@
  * @n: the int to check
  * Return: the absolute value if int
  */
-int_abs(int n)
+int _abs(int n)
 {
 	if (n >= 0)
 	{
 		return (n);
 	}
-	return (-n);
+	else
+		return (-n);
 }
