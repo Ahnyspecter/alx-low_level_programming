f4360d96acf3dee84092703bdddefd722be430e1
ahnspecter <mnak@gmail.com>
2023-02-27 16:34:58 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,12 +24,13 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-')
 			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n = n * 10 + digits;
+			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
@@ -37,7 +38,9 @@ int _atoi(char *s)
 		}
 		i++;
 	}
+	
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
