415c79e0c80e4087855262a480c9717c12f34c03
ahnspecter <mnak@gmail.com>
2023-03-01 11:59:12 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reverse_arrey - reverses an array of integers
+ * reverse_array - reverses an array of integers
  * @a: array
  * @n: number of elements of the array
  * Return: rev
