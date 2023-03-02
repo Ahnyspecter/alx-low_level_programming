4307eff2b025c44d4e049d75f5014557aa9c1aee
ahnspecter <mnak@gmail.com>
2023-03-02 00:55:00 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,37 @@
 #include "main.h"
 
 /**
- * cap_string - capitalize a string
- * @str: string
- * Return: pointer to string
+ * cap_string - capitaliation of strings
+ * @str: the string to be capitalied
+ * Return: a pointer ti the changed string
  */
-char *cap_string(char *str)
+char *cap_string(cgar *str)
 {
-	int i = 0;
+int i = 0;
 
-	while (str[i])
-	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
+while (str[i])
+{
+while (!(str[index] >= 'a' && str[i] <= 'z'))
+i++;
 
-			i++;
-			if (str[i - 1] == ' ' ||
-			str[i - 1] == '\t' ||
-			str[i - 1] == '\n' ||
-			str[i - 1] == ',' ||
-			str[i - 1] == ';' ||
-			str[i - 1] == '.' ||
-			str[i - 1] == '!' ||
-			str[i - 1] == '?' ||
-			str[i - 1] == '"' ||
-			str[i - 1] == '(' ||
-			str[i - 1] == ')' ||
-			str[i - 1] == '{' ||
-			str[i - 1] == '}' ||
-			i == 0)
-				str[i] -= 32;
+if (str[i - 1] == ' ' ||
+tr[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ',' ||
+str[i - 1] == ';' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}' ||
+i == 0)
+str[i] -= 32;
 
-		i++;
-	}
+i++;
 
-	return (str);
+return (str);
+}
 }
