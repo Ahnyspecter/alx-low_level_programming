d064b8a04ffdce6f729e251cebb1cbb79712730f
ahnspecter <mnak@gmail.com>
2023-03-06 18:01:42 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,28 +1,24 @@
-/*
- * File: 0-memset.c
- * Auth: not
- */
-
 #include "main.h"
 
 /**
  * _memset - Files the first n bytes of the memory area
  *	pointed to by @s with the constant byte @c.
  * @s: A pointer to the memory area to be filled
- * @c: The char to fill the memory area with.
+ * @b: The char to fill the memory area with.
  * @n: The number of bytes to be filled.
  * description _memset over there
  *
  * Return: A pointer to the filled memory area @s
  *
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n);
 {
-	unsigned int index;
-	unsigned char *memory = s, alue = c;
+	int i = 0;
 
-	for (index = 0; inde < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	for (; n > 0; i++)
+	{
+		s[i] =  b;
+		n--;
+	}
+	return (s);
 }
