52aca2a0d0136b43c1825ced0459e780128675c1
Ahnyspecter <mspecter15@gmail.com>
2023-05-22 20:33:18 -0700

    libdynamic.so, main.h

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute of an integer
+ * @n: the int to check
+ * Return: the absolute value if int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+}
